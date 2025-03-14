ccf547286a74df91316186eee0a7c61c107d9f69
jhon <jhonhinostroza1234@gmail.com>
2025-03-14 09:48:40 -0500

    Project publication

---
+++ src/main/java/com/gestion/today/AdministracionGestionInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionGestionInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionGestionInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/today/AdministracionGestionInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.today;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionGestionInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
